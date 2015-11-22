60a6456d9ad5458bb2c3a1b9f711edc5112c0b83
krasch <krasch@kth.se>
2015-11-22 21:17:22 +0100

    Support for playing an album

--- ui/static/js/player.js
+++ ui/static/js/player.js
@@ -2,21 +2,34 @@ socket = createSocket()
 
 player = {
 
-   playTrack: function(trackURI) {
-       socket.send("track," + trackURI);
+   add: function(uri) {
+       if (uri.indexOf("spotify:track") > -1)
+           socket.send(JSON.stringify({"command": "add","uri": uri}));
+       else if (uri.indexOf("spotify:album") > -1) {
+           spotify.getAlbumTracks(uri)
+                  .then(function(tracks) {
+                     for (i in tracks)
+                        socket.send(JSON.stringify({"command": "add","uri": tracks[i]}));
+                  })
+       }
        setTimeout(function() {player.play()}, 100);
-
-       spotify.getTrack(uri)
-              .then(ui.player.update, handleError);
    },
 
    play: function() {
-       socket.send("play");
+       socket.send(JSON.stringify({"command": "play"}));
    },
 
    pause: function() {
-       socket.send("pause");
+       socket.send(JSON.stringify({"command": "pause"}));
    },
+
+   next: function() {
+       socket.send(JSON.stringify({"command": "next"}));
+   },
+
+   clear: function() {
+       socket.send(JSON.stringify({"command": "clear"}));
+   }
 }
 
 function createSocket() {
@@ -28,8 +41,12 @@ function createSocket() {
         };
 
     ws.onmessage = function (evt) {
-      var received_msg = evt.data;
-      console.log("Received: " + received_msg);
+      var msg = JSON.parse(evt.data);
+
+      if (msg["event"] == "track") {
+         spotify.getTrack(msg["uri"])
+                .then(ui.player.update, handleError);
+      }
     };
 
     ws.onclose = function() {
