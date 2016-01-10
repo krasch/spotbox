242208ecf7b69017986b439c59baa1c965cb9adb
Petter Arvidsson <petter.arvidsson@gmail.com>
2016-01-10 21:53:25 +0100

    Change web socket to observable

--- ui/static/js/player.js
+++ ui/static/js/player.js
@@ -1,36 +1,34 @@
 socketUri = "ws://"+window.location.host+"/ws"
 socket = createSocket(socketUri)
 
-player = {
+player = riot.observable()
+player.add = function(uri) {
+    if (uri.indexOf("spotify:track") > -1)
+        socket.send(JSON.stringify({"command": "add","uri": uri}));
+    else if (uri.indexOf("spotify:album") > -1) {
+        spotify.album.listTracks(uri)
+            .then(function(tracks) {
+                for (i in tracks)
+                    socket.send(JSON.stringify({"command": "add","uri": tracks[i]}));
+            })
+    }
+    setTimeout(function() {player.play()}, 100);
+}
 
-   add: function(uri) {
-       if (uri.indexOf("spotify:track") > -1)
-           socket.send(JSON.stringify({"command": "add","uri": uri}));
-       else if (uri.indexOf("spotify:album") > -1) {
-           spotify.album.listTracks(uri)
-                  .then(function(tracks) {
-                     for (i in tracks)
-                        socket.send(JSON.stringify({"command": "add","uri": tracks[i]}));
-                  })
-       }
-       setTimeout(function() {player.play()}, 100);
-   },
+player.play = function() {
+    socket.send(JSON.stringify({"command": "play"}));
+}
 
-   play: function() {
-       socket.send(JSON.stringify({"command": "play"}));
-   },
+player.pause = function() {
+    socket.send(JSON.stringify({"command": "pause"}));
+},
 
-   pause: function() {
-       socket.send(JSON.stringify({"command": "pause"}));
-   },
+player.next = function() {
+    socket.send(JSON.stringify({"command": "next"}));
+}
 
-   next: function() {
-       socket.send(JSON.stringify({"command": "next"}));
-   },
-
-   clear: function() {
-       socket.send(JSON.stringify({"command": "clear"}));
-   }
+player.clear = function() {
+    socket.send(JSON.stringify({"command": "clear"}));
 }
 
 function createSocket(uri) {
@@ -38,22 +36,22 @@ function createSocket(uri) {
     ws = new WebSocket(uri);
 
     ws.onopen = function() {
-          console.log("Socket opened.")
-        };
+        console.log("Socket opened.")
+    };
 
     ws.onmessage = function (evt) {
-      var msg = JSON.parse(evt.data);
+        var msg = JSON.parse(evt.data);
 
-      if (msg["event"] == "track")
-         ui.player.update(msg["uri"])
-      else if (msg["event"] == "queued")
-         ui.player.displayQueue(msg["tracks"])
-      else
-        console.log(msg);
+        if (msg["event"] == "track")
+            player.trigger("track", msg["uri"])
+        else if (msg["event"] == "queued")
+            player.trigger("queued", msg["tracks"])
+        else
+            console.log(msg);
     };
 
     ws.onclose = function() {
-      console.log("Socket closed.");
+        console.log("Socket closed.");
     };
 
     return ws;
